4e5ee1823e0340b3f4ff0171e9856003aa16478f
Riya Joshi <joshiriya467@gmail.com>
2026-05-05 20:52:29 +0530

    Updated file
    Fixed loop condition in getMaxIndex method to include end index.

--- DAY12/selection_sort.java
+++ DAY12/selection_sort.java
@@ -16,7 +16,7 @@ public class selection_sort {
   }
   public static int getMaxIndex(int[] arr, int start, int end){
     int max = start;
-    for (int i = start; i < end; i++){
+    for (int i = start; i <= end; i++){
       if(arr[max]<arr[i]){
         max= i;
       }  
